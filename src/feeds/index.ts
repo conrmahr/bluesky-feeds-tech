62bb1d8544687ca57fb60a709f00953e27be6725
conrmahr <conor@hireconor.com>
2023-06-08 09:54:04 -0500

    wip: remove minial and love feeds

--- src/feeds/index.ts
+++ src/feeds/index.ts
@@ -1,8 +1,6 @@
-import love from "./love/index.js";
-import minimal from "./minimal/index.js";
+import tech from "./tech/index.js";
 import { Feed } from "./types.js";
 
-// const feeds: Feed[] = [minimal, love];
-const feeds: Feed[] = [minimal];
+const feeds: Feed[] = [tech];
 
 export default feeds;
